7016c7ee3c6eb48e109b8a281512f5534c5221ee
lisethav <lisethav@gmail.com>
2021-12-23 02:59:15 -0500

    fix error

--- models/state.py
+++ models/state.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-import models
-from models.base_model import Base, BaseModel
+"""This is the state class"""
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import backref, relationship
+from sqlalchemy.orm import relationship
 from models.city import City
-from models import storage
+import models
+import os
+
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullbase=False)
-    cities = relationship('City', backref=backref(
-                        "state", cascade="all, delete"))
-    
-    @property
-    def cities(self):
-        """Returns cities obj
-        """
-        new_list = []
-        for city in models.storage.all(City).values():
-            if city.state_id == self.id:
-                new_list.append(city)
-        return new_list
+    name = Column(String(128), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship(
+            'City', backref='state', cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            """Returns cities obj """
+            new_list = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    new_list.append(city)
+            return (new_list)
