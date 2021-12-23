be04d8ce0d9a5c298307f85f9c99dd68e59ef3c9
lisethav <lisethav@gmail.com>
2021-12-23 02:06:53 -0500

    state

--- models/state.py
+++ models/state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from os import getenv
 import models
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
@@ -15,12 +14,11 @@ class State(BaseModel, Base):
     cities = relationship('City', backref=backref(
                         "state", cascade="all, delete"))
     
-    if getenv("HBNB_TYPE_STORAGE") != "db":
-        @property
-        def cities(self):
-            """Returns all cities in state"""
-            new_list = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    new_list.append(city)
-            return new_list
+    @property
+    def cities(self):
+        """Returns all cities in state"""
+        new_list = []
+        for city in models.storage.all(City).values():
+            if city.state_id == self.id:
+                new_list.append(city)
+        return new_list
